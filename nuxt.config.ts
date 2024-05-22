9629e7e56a7f3b41104359266674f6977ce7887e
Martins Zeltins <martins.zeltins@arkbauer.com>
2024-05-22 15:26:13 +0300

    Initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: ["@nuxtjs/i18n"],
-  i18n: {
+    modules: ["@nuxtjs/i18n"],
+
+    i18n: {
         locales: ['en', 'fr'].map(locale => {
-        return {
-            code: locale,
-            file: `${locale}.json`,
-        }
-    }),
-    lazy: false,
-    langDir: 'lang',
-    defaultLocale: 'en',
-    detectBrowserLanguage: false
-  }
+            return {
+                code: locale,
+                file: `${locale}.json`,
+            }
+        }),
+        
+        langDir: 'lang',
+        defaultLocale: 'en',
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,27 @@
 <template>
-    <HelloWorld />
+    <div>{{ $t('welcome') }}</div>
 
-    <button @click="updateLocale">Change</button>
+    <button @click="updateLocale">
+        Change Locale
+    </button>
 </template>
 
 <script setup lang="ts">
-  const { locale, setLocale } = useI18n()
+    const { locale, setLocale } = useI18n()
 
-  const updateLocale = () => {
-    if (locale.value === 'en') {
-      setLocale('fr')
-    } else {
-      setLocale('en')
+    const updateLocale = () => {
+        if (locale.value === 'en') {
+            setLocale('fr')
+        } else {
+            setLocale('en')
+        }
     }
-  }
+
+    onMounted(() => {
+        console.log('onMounted')
+    })
+
+    onUnmounted(() => {
+        console.log('onUnmounted')
+    })
 </script>
